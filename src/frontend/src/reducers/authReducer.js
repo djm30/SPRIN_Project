47e5e4bb7cdbe94a3cc94ab9c9a194d6da24fa76
Dylan Morrison <dylanjmorrison30@gmail.com>
2022-09-24 10:37:42 +0100

    notification and auth reducers

--- src/frontend/src/reducers/authReducer.js
+++ src/frontend/src/reducers/authReducer.js
@@ -1,11 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
+import {
+  login as loginRequest,
+  logout as logoutRequest,
+} from "../services/authService";
 
 let initialState = null;
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    login(state, action) {
+      return action.payload;
+    },
+    logout(state, action) {
+      return null;
+    },
+  },
 });
+
+const { login, logout } = authSlice.actions;
+
+const deleteCookie = (name) => {
+  document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+};
+
+export const SubmitLogin = (email, password) => {
+  return async (dispatch) => {
+    try {
+      const user = await loginRequest(email, password);
+      dispatch(login(user));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
+
+export const submitLogout = () => {
+  return async (dispatch) => {
+    try {
+      await logoutRequest();
+      deleteCookie("connect.sid");
+      dispatch(logout());
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
 
 export default authSlice.reducer;
--- src/frontend/src/reducers/notificationReducer.js
+++ src/frontend/src/reducers/notificationReducer.js
@@ -1,11 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-let initialState = { error: false, message: "" };
+let timeout;
+
+let initialState = {
+  error: false,
+  message: "",
+};
 
 const notificationSlice = createSlice({
   name: "notification",
   initialState,
-  reducers: {},
+  reducers: {
+    setMessage(state, action) {
+      return action.payload;
+    },
+    resetMessage(state, action) {
+      return initialState;
+    },
+  },
 });
+
+export const setNotification = (message, error, time = 5) => {
+  return (dispatch) => {
+    clearTimeout(timeout);
+    dispatch(setMessage({ message, error }));
+    timeout = setTimeout(() => {
+      dispatch(resetMessage());
+    }, time * 1000);
+  };
+};
+
+const { setMessage, resetMessage } = notificationSlice.actions;
 
 export default notificationSlice.reducer;
