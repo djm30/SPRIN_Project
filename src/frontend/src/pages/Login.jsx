9bdcf3e94a29dfc48be0158b62ad1cc2f7655d4d
Ethan Gourley <ethanjackgourley@gmail.com>
2022-10-06 19:54:46 +0100

    Progress to login page

--- src/frontend/src/pages/Login.jsx
+++ src/frontend/src/pages/Login.jsx
@@ -4,10 +4,42 @@ import Navbar from "../components/UI/Navbar/Navbar";
 const Login = () => {
   return (
     <div>
-      <Navbar transparent={false} />
-      Login
-      <form></form>
+    <section
+        id={styles.cta}
+        className="h-screen w-screen bg-darkblue-100 bg-no-repeat   rounded-bl-[30%] relative overflow-hidden"
+      ></section>
+      
+      {/* NAVBAR */}
+      <Navbar transparent={true} />
+        {/* HEADING AND BUTTONS */}
+        <ContentContainer className="mt-20 space-y-20 flex flex-col items-center">
+          <div className="space-y-6 mr-32">
+            <h1 className="text-6xl text-white font-display">
+              LOGIN
+            </h1>
+            </div>
+            </ContentContainer>
+      <form>
+      <div className="space-x-16">
+          <h2>
+            Email:
+          </h2>
+          <input className="bg-skyblue-200 text-lg text-white shadow-sm px-10 py-3 rounded-3xl type='Email'" />
+          <h2>
+            Password:
+          </h2>
+          <input className="bg-skyblue-200 text-lg text-white shadow-sm px-10 py-3 rounded-3xl type='Email'" />
+          <a className="bg-skyblue-200 text-lg hover:bg-skyblue-300 transition-all text-white shadow-sm px-10 py-3 rounded-3xl select-none">
+              Login
+            </a>
+            <a className="bg-skyblue-200 text-lg hover:bg-skyblue-300 transition-all text-white shadow-sm px-10 py-3 rounded-3xl select-none">
+              Register
+            </a>
+        </div>
+      </form>
+      
     </div>
+   
   );
 };
 
