338aab0f10a38af4ebd38b0d98dc1b0e9ad7e94c
Dylan Morrison <dylanjmorrison30@gmail.com>
2022-10-14 11:09:21 +0100

    fixed weird issue of modal closing on click

--- src/frontend/src/components/UI/Modal/Modal.jsx
+++ src/frontend/src/components/UI/Modal/Modal.jsx
@@ -2,21 +2,46 @@ import React, { useRef } from "react";
 import { createPortal } from "react-dom";
 import Card from "../Card";
 
+/**
+ *
+ * @param {*} param0
+ * @returns
+ */
 const Modal = ({ open, setOpen, children, className }) => {
-  let clicked;
+  const backgroundRef = useRef(null);
+  const modalRef = useRef(null);
+
+  // Stores value of mouseDown position
+  let clickTarget;
+
+  // Gets the position of the curser when it is initially clicked
+  const mouseDown = (e) => {
+    console.log("triggered");
+    clickTarget = e.target;
+  };
+
+  //Will close the modal only if the original mouseDown event was targeted at the modal background
+  const mouseUp = (e) => {
+    // Checks that both the mouse click origin and destination came are from the modal background
+    if (
+      clickTarget === backgroundRef.current &&
+      e.target === backgroundRef.current
+    ) {
+      setOpen(!open);
+    }
+  };
 
   return open
     ? createPortal(
         // BACKGROUND DIV
         <div
+          ref={backgroundRef}
           className="w-full h-screen bg-modal  fixed top-0 left-0 z-10 flex justify-center pt-10"
-          onClick={(e) => {
-            e.stopPropagation();
-            setOpen(!open);
-          }}
+          onMouseDown={mouseDown}
+          onMouseUp={mouseUp}
         >
           {/* CONTENT DIV */}
-          <Card className={`relative my-5 ${className}`}>
+          <Card className={`relative my-5 ${className}`} useRef={modalRef}>
             {/* CLOSE BUTTON */}
             <span
               className="absolute top-3 right-3 text-4xl cursor-pointer select-none hover:text-gray-500 transition-all"
