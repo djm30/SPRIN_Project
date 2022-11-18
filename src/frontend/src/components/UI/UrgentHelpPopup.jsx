58d32caf1bfd7c4185e37fa6a43cbb8cab79b9ac
Mathew <msteele09@qub.ac.uk>
2022-11-18 15:59:18 +0000

    Congifugred Popup Timer

--- src/frontend/src/components/UI/UrgentHelpPopup.jsx
+++ src/frontend/src/components/UI/UrgentHelpPopup.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 export default function PopUp() {
+  const timerId = setInterval(() => {
+    setShowModal(true)
+  }, 3000);
   const [showModal, setShowModal] = React.useState(false);
   return (
     <>
-      <button
-        className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        Test Pop Up
-      </button>
       {showModal ? (
         <>
           <div
