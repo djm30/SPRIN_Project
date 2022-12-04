18d92fae4b70706bc2ecb6e90fc618128afc59ca
Mathew <msteele09@qub.ac.uk>
2022-12-04 22:13:35 +0000

    Code

--- src/frontend/src/components/Partners/JoinTodayPop.jsx
+++ src/frontend/src/components/Partners/JoinTodayPop.jsx
@@ -5,7 +5,7 @@ export default function JoinPopUp() {
   return (
     <>
       <button
-        className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        className="bg-blue-500 text-white active:bg-blue-600 font-bold text-sm px-6 py-3 rounded-3xl shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
         type="button"
         onClick={() => setShowModal(true)}
       >
